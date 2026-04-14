0fba8aa93225d32b7a2d72e52a1192ccb2e51080
LawrenceNorman <LawrenceNorman@hotmail.com>
2026-04-13 19:49:09 -0700

    Fixups
    Fixups

--- frameworks/macos/audiokit-midi-synth/Package.swift
+++ frameworks/macos/audiokit-midi-synth/Package.swift
@@ -3,13 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "AudioKitMIDISynth",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .executable(name: "AudioKitMIDISynth", targets: ["AudioKitMIDISynth"])
     ],
     dependencies: [
         .package(url: "https://github.com/AudioKit/AudioKit.git", from: "5.6.0"),
-        .package(url: "https://github.com/AudioKit/AudioKitUI.git", from: "0.4.0"),
+        .package(url: "https://github.com/AudioKit/AudioKitUI.git", from: "0.3.0"),
         .package(url: "https://github.com/AudioKit/SoundpipeAudioKit.git", from: "5.6.0")
     ],
     targets: [
